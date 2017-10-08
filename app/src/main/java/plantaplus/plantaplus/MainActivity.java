15567e182d95ca0e721e99a49b8a873a92479d70
Rodrigo Aguiar <rodrigoaguiar0@gmail.com>
2017-10-08 17:38:22 -0300

    Captura de dados dos formulários e ação do botão de login

--- app/src/main/java/plantaplus/plantaplus/MainActivity.java
+++ app/src/main/java/plantaplus/plantaplus/MainActivity.java
@@ -1,7 +1,11 @@
 package plantaplus.plantaplus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+
+//        bLogin.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                Intent loginIntent = new Intent(MainActivity.this, );
+//                MainActivity.this.startActivity(loginIntent);
+//            }
+//        });
     }
 }
