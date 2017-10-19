3cb575a63ff727eff2bc72cccbcea72c6ef90a46
rbpaludo <rbpaludo@gmail.com>
2017-10-19 13:16:50 -0200

    Atualizacao Branch

---
+++ app/src/main/java/plantaplus/plantaplus/model/Usuario.java
@@ -0,0 +1,18 @@
+package plantaplus.plantaplus.model;
+
+import java.util.Date;
+
+/**
+ * Created by ASUS on 18/10/2017.
+ */
+
+public class Usuario {
+    private String username;
+    private String email;
+    private String senha;
+    private Date data;
+    private int idPessoa;
+    private String nome;
+
+
+}
