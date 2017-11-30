eadbb5bf161d2323f4ab9842910b3d89f65bf1ee
Rhenanrk <rhenan_konrad@hotmail.com>
2017-11-30 01:15:13 -0200

    Criação das telas de informaçào detalhada da planta e criação da tela para deletar a planta
    Foram criadas apenas as telas com seus respectivos botões. Não foram adicionados os códicos referentes às açoes proveniente do banco de dados (exibir as inormações das plantas e deletar a planta)

--- app/src/main/java/plantaplus/plantaplus/InformacaoDetalhadaActivity.java
+++ app/src/main/java/plantaplus/plantaplus/InformacaoDetalhadaActivity.java
@@ -4,12 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class InformacaoDetalhadaActivity extends AppCompatActivity{
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_informacao_detalhada);
 
+    final Button bDeletar = (Button) findViewById(R.id.bDeletar);
 
-
-
+    bDeletar.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent it = new Intent(InformacaoDetalhadaActivity.this, DeletarPlantaActivity.class);
+            startActivity(it);
+        }
+    });
+    }
 }
